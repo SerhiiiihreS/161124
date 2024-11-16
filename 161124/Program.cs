822f00692b75363699bc8443e3fa2384ce3d01d7
Serhii <0992830717s@gmail.com>
2024-11-16 10:19:47 +0100

    Add project files.

---
+++ 161124/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _161124
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void CurTime()
+            {
+                DateTime time = DateTime.Now;
+                Console.WriteLine(time.Hour + ":" + time.Minute + ":" + time.Second);
+            }
+
+            void CurDate()
+            {
+                DateTime date = DateTime.Now;
+                Console.WriteLine(date);           
+            }
+
+            Action action = new Action(CurTime);
+            Action action1 = new Action(CurDate);
+
+            action.Invoke();
+            action1.Invoke();
+        }
+    }
+}
